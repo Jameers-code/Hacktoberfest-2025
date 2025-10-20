3a33bd2702d409d7dbb39008bd0c96f757c38a00
Jameer shaik <Jameer.xxvi@gmail.com>
2025-10-20 11:49:21 +0530

    Create maxSubarray.cpp

---
+++ maxSubarray.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum=0;
+        // for a frist element ke liye
+        int maxi=nums[0];
+        int n=nums.size();
+
+        for(int i=0;i<n;i++){
+            sum=sum+nums[i];
+
+            maxi=max(maxi,sum);
+
+            if(sum<0)
+            sum=0;
+        }
+
+        // return max karo
+
+        return maxi;
+
+
+        
+    }
+};
